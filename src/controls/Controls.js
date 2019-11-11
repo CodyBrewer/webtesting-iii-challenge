1a90164931baac856e1165b01f5cca2a3a3e2828
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-11 11:53:27 -0700

    app completely using redux store only, no state and all toggleFunctions outside of store

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleLocked, toggleClosed } from '../store/actions';
 
-const Controls = props => {
-  const { locked, closed, toggleLocked, toggleClosed } = props;
+const Controls = () => {
+  // const { locked, closed, toggleLocked, toggleClosed } = props;
+  const locked = useSelector(state => state.locked);
+  const closed = useSelector(state => state.closed);
+  const dispatch = useDispatch();
 
   return (
-    <div className="controls panel">
-      <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
+    <div className='controls panel'>
+      <button
+        disabled={!closed}
+        onClick={() => dispatch(toggleLocked())}
+        className='toggle-btn'>
         {locked ? 'Unlock Gate' : 'Lock Gate'}
       </button>
-      <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
+      <button
+        disabled={locked}
+        onClick={() => dispatch(toggleClosed())}
+        className='toggle-btn'>
         {closed ? 'Open Gate' : 'Close Gate'}
       </button>
     </div>
--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,44 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import Display from '../display/Display';
 import Controls from '../controls/Controls';
-import { toggleClosed, toggleLocked } from '../store/actions';
 
 class Dashboard extends React.Component {
-  // state = {
-  //   locked: false,
-  //   closed: false,
-  // };
-
   render() {
-    const { closed, locked, toggleClosed, toggleLocked } = this.props;
-
     return (
       <>
         <Display />
-        <Controls
-          locked={locked}
-          closed={closed}
-          toggleLocked={toggleLocked}
-          toggleClosed={toggleClosed}
-        />
+        <Controls />
       </>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    locked: state.locked,
-    closed: state.closed
-  };
-};
-const mapDispatchToProps = {
-  toggleLocked,
-  toggleClosed
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Dashboard);
+export default Dashboard;
