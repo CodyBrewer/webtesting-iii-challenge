b9e636b7435a105084044a103c14b4d3f0936ee3
CodyBrewer <brewer.cody.m@gmail.com>
2019-11-11 11:16:40 -0700

    setting up redux

---
+++ src/store/reducers/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+  locked: false,
+  closed: false
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
